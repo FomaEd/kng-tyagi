6729b6429c6bfbcceb0f9e25e1b0e6665019da1d
FomaEd <164784372+FomaEd@users.noreply.github.com>
2026-03-20 14:48:10 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const TelegramBot = require('node-telegram-bot-api');
 
-const token = 'YOUR_TOKEN_HERE';
+const token = process.env.BOT_TOKEN;
 
 const bot = new TelegramBot(token, { polling: true });
 
